b701ed5cb2cbb8b8bfc5b02482e1b0661922004a
Stephen da Conceicao <daconcei472@gmail.com>
2025-07-22 00:20:31 -0400

    Initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
+
+import { resolvers } from "./resolvers";
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
+      return {
+        dataSources: {},
+      };
+    },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
