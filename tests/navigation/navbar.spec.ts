aa994e6d450933f106c182ab16a1a02bb9ffec7e
Stephen da Conceicao <daconcei472@gmail.com>
2025-08-26 14:46:23 -0400

    better org

--- tests/navigation/navbar.spec.ts
+++ tests/navigation/navbar.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Grass Type Navigation", () => {
+test.describe("Navbar", () => {
   test("should navigate to grass type and load Bulbasaur details", async ({
     page,
   }) => {
