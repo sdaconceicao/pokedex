8c54747eb494ffcd650e1fd7817158125cb2a1b7
Stephen da Conceicao <sdaconceicao@ncscredit.com>
2026-06-10 17:41:11 -0400

    linty

--- backend/pokedex-rest/test/vitest-e2e.config.ts
+++ backend/pokedex-rest/test/vitest-e2e.config.ts
@@ -1,17 +1,17 @@
-import swc from "unplugin-swc";
-import { defineConfig } from "vitest/config";
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
     swc.vite({
-      module: { type: "es6" },
+      module: { type: 'es6' },
     }),
   ],
   test: {
     globals: true,
-    root: ".",
-    include: ["**/*.e2e-spec.ts"],
-    environment: "node",
-    setupFiles: ["./vitest.setup.ts"],
+    root: '.',
+    include: ['**/*.e2e-spec.ts'],
+    environment: 'node',
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
