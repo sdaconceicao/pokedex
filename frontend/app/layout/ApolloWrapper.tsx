ed6821bf5d51f3422ed14e08bd5fc93228ca121d
Stephen da Conceicao <sdaconceicao@ncscredit.com>
2026-07-14 00:31:14 -0400

    fix apollo tsc

--- frontend/app/layout/ApolloWrapper.tsx
+++ frontend/app/layout/ApolloWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 import { client } from "../lib/apollo-client";
 
 export default function ApolloWrapper({ children }: { children: React.ReactNode }) {
--- frontend/app/layout/HomePageLayout/usePokemonUnifiedQuery.ts
+++ frontend/app/layout/HomePageLayout/usePokemonUnifiedQuery.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 import { useEffect, useMemo, useState } from "react";
 import { mapSpecialToTitle } from "@/layout/Navbar/Navbar.util";
 import {
