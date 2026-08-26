3b889776f13393e5c4707caa8d15fabe2cbf5fae
Stephen da Conceicao <sdaconceicao@ncscredit.com>
2026-08-26 14:25:04 -0400

    trigger build

--- backend/pokedex-rest/src/auth/dtos/password-reset-request.dto.ts
+++ backend/pokedex-rest/src/auth/dtos/password-reset-request.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class PasswordResetRequestDto {
   @ApiProperty({
     example: 'ash.ketchum@pallet.town',
-    description: 'Email address to send a password reset link to',
+    description: 'Email address to send password reset link to',
   })
   email: string;
 }
