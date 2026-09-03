7f5aa4dd854b20d1f6250fa73634b118b0a70a74
Stephen da Conceicao <daconcei472@gmail.com>
2026-09-02 23:09:47 -0400

    format

--- backend/pokedex-rest/src/types/fastify-multipart.d.ts
+++ backend/pokedex-rest/src/types/fastify-multipart.d.ts
@@ -2,9 +2,7 @@ import type { BusboyConfig } from '@fastify/busboy';
 
 declare module 'fastify/types/request' {
   interface FastifyRequest {
-    file: (
-      options?: Omit<BusboyConfig, 'headers'>,
-    ) => Promise<
+    file: (options?: Omit<BusboyConfig, 'headers'>) => Promise<
       | {
           toBuffer: () => Promise<Buffer>;
           mimetype: string;
